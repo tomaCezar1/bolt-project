8c5f6e72e1212e2e272b708294c9d910023ee455
tomaCezar1 <71145334+tomaCezar1@users.noreply.github.com>
2026-04-23 11:00:05 +0300

    update app.tsx

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import Navigation from './components/Navigation';
+import Hero from './components/Hero';
+import About from './components/About';
+import Services from './components/Services';
+import Testimonials from './components/Testimonials';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-white">
+      <Navigation />
+      <Hero />
+      <About />
+      <Services />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
